f006177a9f9a49215d5e65b658f4a9485fc8c779
Truong1103 <gymptw1@gmail.com>
2025-07-17 16:35:23 +0700

    fix loi lan 6

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import dynamic from "next/dynamic";
+import dynamicImport from "next/dynamic";
 export const dynamic = "force-dynamic";
-const HomePage = dynamic(() => import("./HomePageContent"), { ssr: false });
+const HomePage = dynamicImport(() => import("./HomePageContent"), { ssr: false });
 export default function Page() { return <HomePage /> }
